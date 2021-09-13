12d9d08fed2cc45d435400d8f8354feef58ab78d
SamuelDiaz970 <54907920+SamuelDiaz970@users.noreply.github.com>
2021-09-12 21:09:30 -0600

    Update q7_UART.c

--- STEAM_FlightSoftware/Unit_Building/q7_UART.c
+++ STEAM_FlightSoftware/Unit_Building/q7_UART.c
@@ -1,42 +1,35 @@
 /******************************************************************************
-* Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
-/******************************************************************************/
+/****************************************************************************/
 /**
 *
-* @file xuartlite_polled_example.c
+* @file     xuartps_polled_example.c
 *
-* This file contains a design example using the UartLite driver (XUartLite) and
-* hardware device using the polled mode.
+* This file contains an example using the XUartPs driver in polled mode.
+*
+* This function sends data and expects to receive the data through the device
+* using the local loopback mode.
 *
 * @note
-*
-* The user must provide a physical loopback such that data which is
-* transmitted will be received.
+* If the device does not work properly, the example may hang.
 *
 * MODIFICATION HISTORY:
 * <pre>
-* Ver   Who  Date	 Changes
-* ----- ---- -------- -----------------------------------------------
-* 1.00a jhl  02/13/02 First release
-* 1.00a sv   06/13/05 Minor changes to comply to Doxygen and coding guidelines
-* 2.00a ktn  10/20/09 Updated this example to wait for valid data in receive
-*		      fifo instead of Tx fifo empty to update receive buffer
-*		      and minor changes as per coding guidelines.
-* 3.2   ms   01/23/17 Added xil_printf statement in main function to
-*                     ensure that "Successfully ran" and "Failed" strings
-*                     are available in all examples. This is a fix for
-*                     CR-965028.
+* Ver   Who    Date     Changes
+* ----- ------ -------- -----------------------------------------------
+* 1.00a  drg/jz 01/13/10 First Release
+* 1.03a  sg     07/16/12 Modified the device ID to use the first Device Id
+*			Removed the printf at the start of the main
 * </pre>
 ******************************************************************************/
 
 /***************************** Include Files *********************************/
 
 #include "xparameters.h"
-#include "xstatus.h"
-#include "xuartlite.h"
+#include "xuartps.h"
 #include "xil_printf.h"
 
 /************************** Constant Definitions *****************************/
@@ -46,159 +39,160 @@
  * xparameters.h file. They are defined here such that a user can easily
  * change all the needed parameters in one place.
  */
-#define UARTLITE_DEVICE_ID	XPAR_UARTLITE_0_DEVICE_ID
+#define UART_DEVICE_ID              XPAR_XUARTPS_0_DEVICE_ID
 
 /*
  * The following constant controls the length of the buffers to be sent
- * and received with the UartLite, this constant must be 16 bytes or less since
- * this is a single threaded non-interrupt driven example such that the
- * entire buffer will fit into the transmit and receive FIFOs of the UartLite.
+ * and received with the device, this constant must be 32 bytes or less since
+ * only as much as FIFO size data can be sent or received in polled mode.
  */
-#define TEST_BUFFER_SIZE 16
+#define TEST_BUFFER_SIZE 32
 
 /**************************** Type Definitions *******************************/
 
-
 /***************** Macros (Inline Functions) Definitions *********************/
-
 
 /************************** Function Prototypes ******************************/
 
-int UartLitePolledExample(u16 DeviceId);
+int UartPsPolledExample(u16 DeviceId);
 
 /************************** Variable Definitions *****************************/
 
-XUartLite UartLite;		/* Instance of the UartLite Device */
+XUartPs Uart_PS;		/* Instance of the UART Device */
 
 /*
  * The following buffers are used in this example to send and receive data
- * with the UartLite.
+ * with the UART.
  */
-u8 SendBuffer[TEST_BUFFER_SIZE];	/* Buffer for Transmitting Data */
-u8 RecvBuffer[TEST_BUFFER_SIZE];	/* Buffer for Receiving Data */
+static u8 SendBuffer[TEST_BUFFER_SIZE];	/* Buffer for Transmitting Data */
+static u8 RecvBuffer[TEST_BUFFER_SIZE];	/* Buffer for Receiving Data */
+
 
 /*****************************************************************************/
 /**
 *
-* Main function to call the Uartlite polled example.
+* Main function to call the Uart Polled mode example.
 *
 *
-* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE
 *
-* @note		None.
+* @note		None
 *
 ******************************************************************************/
+#ifndef TESTAPP_GEN
 int main(void)
 {
 	int Status;
 
+
 	/*
-	 * Run the UartLite polled example, specify the Device ID that is
+	 * Run the Uart_PS polled example , specify the the Device ID that is
 	 * generated in xparameters.h
 	 */
-	Status = UartLitePolledExample(UARTLITE_DEVICE_ID);
+	Status = UartPsPolledExample(UART_DEVICE_ID);
 	if (Status != XST_SUCCESS) {
-		xil_printf("Uartlite polled Example Failed\r\n");
+		xil_printf("UART Polled Mode Example Test Failed\r\n");
 		return XST_FAILURE;
 	}
 
-	xil_printf("Successfully ran Uartlite polled Example\r\n");
+	xil_printf("Successfully ran UART Polled Mode Example Test\r\n");
 	return XST_SUCCESS;
-
 }
-
-
-/****************************************************************************/
+#endif
+/*****************************************************************************/
 /**
-* This function does a minimal test on the UartLite device and driver as a
-* design example. The purpose of this function is to illustrate
-* how to use the XUartLite component.
 *
-* This function sends data and expects to receive the data through the UartLite
-* such that a  physical loopback must be done with the transmit and receive
-* signals of the UartLite.
+* This function does a minimal test on the XUartPs device in polled mode.
 *
-* This function polls the UartLite and does not require the use of interrupts.
+* This function sends data and expects to receive the data through the UART
+* using the local loopback mode.
 *
-* @param	DeviceId is the Device ID of the UartLite and is the
-*		XPAR_<uartlite_instance>_DEVICE_ID value from xparameters.h.
 *
-* @return	XST_SUCCESS if successful, XST_FAILURE if unsuccessful.
+* @param	DeviceId is the unique device id from hardware build.
 *
+* @return	XST_SUCCESS if successful, XST_FAILURE if unsuccessful
 *
 * @note
-*
-* This function calls the UartLite driver functions in a blocking mode such that
-* if the transmit data does not loopback to the receive, this function may
-* not return.
+* This function polls the UART, it may hang if the hardware is not
+* working correctly.
 *
 ****************************************************************************/
-int UartLitePolledExample(u16 DeviceId)
+int UartPsPolledExample(u16 DeviceId)
 {
 	int Status;
+	XUartPs_Config *Config;
 	unsigned int SentCount;
-	unsigned int ReceivedCount = 0;
-	int Index;
+	unsigned int ReceivedCount;
+	u16 Index;
+	u32 LoopCount = 0;
 
 	/*
-	 * Initialize the UartLite driver so that it is ready to use.
+	 * Initialize the UART driver so that it's ready to use.
+	 * Look up the configuration in the config table, then initialize it.
 	 */
-	Status = XUartLite_Initialize(&UartLite, DeviceId);
+	Config = XUartPs_LookupConfig(DeviceId);
+	if (NULL == Config) {
+		return XST_FAILURE;
+	}
+
+	Status = XUartPs_CfgInitialize(&Uart_PS, Config, Config->BaseAddress);
 	if (Status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 
-	/*
-	 * Perform a self-test to ensure that the hardware was built correctly.
-	 */
-	Status = XUartLite_SelfTest(&UartLite);
+	/* Check hardware build. */
+	Status = XUartPs_SelfTest(&Uart_PS);
 	if (Status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 
+	/* Use local loopback mode. */
+	XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_LOCAL_LOOP);
+
 	/*
-	 * Initialize the send buffer bytes with a pattern to send and the
-	 * the receive buffer bytes to zero.
+	 * Initialize the send buffer bytes with a pattern and zero out
+	 * the receive buffer.
 	 */
 	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-		SendBuffer[Index] = Index;
+		SendBuffer[Index] = '0' + Index;
 		RecvBuffer[Index] = 0;
 	}
 
-	/*
-	 * Send the buffer through the UartLite waiting til the data can be sent
-	 * (block), if the specified number of bytes was not sent successfully,
-	 * then an error occurred.
-	 */
-	SentCount = XUartLite_Send(&UartLite, SendBuffer, TEST_BUFFER_SIZE);
+	/* Block sending the buffer. */
+	SentCount = XUartPs_Send(&Uart_PS, SendBuffer, TEST_BUFFER_SIZE);
 	if (SentCount != TEST_BUFFER_SIZE) {
 		return XST_FAILURE;
 	}
 
 	/*
-	 * Receive the number of bytes which is transferred.
-	 * Data may be received in fifo with some delay hence we continuously
-	 * check the receive fifo for valid data and update the receive buffer
-	 * accordingly.
+	 * Wait while the UART is sending the data so that we are guaranteed
+	 * to get the data the 1st time we call receive, otherwise this function
+	 * may enter receive before the data has arrived
 	 */
-	while (1) {
-		ReceivedCount += XUartLite_Recv(&UartLite,
-					   RecvBuffer + ReceivedCount,
-					   TEST_BUFFER_SIZE - ReceivedCount);
-		if (ReceivedCount == TEST_BUFFER_SIZE) {
-			break;
-		}
+	while (XUartPs_IsSending(&Uart_PS)) {
+		LoopCount++;
+	}
+
+	/* Block receiving the buffer. */
+	ReceivedCount = 0;
+	while (ReceivedCount < TEST_BUFFER_SIZE) {
+		ReceivedCount +=
+			XUartPs_Recv(&Uart_PS, &RecvBuffer[ReceivedCount],
+				      (TEST_BUFFER_SIZE - ReceivedCount));
 	}
 
 	/*
-	 * Check the receive buffer data against the send buffer and verify the
-	 * data was correctly received.
+	 * Check the receive buffer against the send buffer and verify the
+	 * data was correctly received
 	 */
 	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
 		if (SendBuffer[Index] != RecvBuffer[Index]) {
 			return XST_FAILURE;
 		}
 	}
+
+	/* Restore to normal mode. */
+	XUartPs_SetOperMode(&Uart_PS, XUARTPS_OPER_MODE_NORMAL);
 
 	return XST_SUCCESS;
 }
