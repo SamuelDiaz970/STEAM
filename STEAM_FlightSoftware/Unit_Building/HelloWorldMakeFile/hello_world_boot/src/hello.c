cbe009e43e7d321bceb85c6138e3616147df835c
Kanin-McGuire <85193693+Kanin-McGuire@users.noreply.github.com>
2021-11-28 15:11:38 -0800

    working

---
+++ STEAM_FlightSoftware/Unit_Building/HelloWorldMakeFile/hello_world_boot/src/hello.c
@@ -0,0 +1,24 @@
+#include "hello.h"
+#include<stdio.h>
+
+int hello(){
+	/*FILE *fp;
+ 
+	fp = fopen("/tmp/hello.txt", "w");
+
+	if(fp == NULL) {
+		printf("file can't be opened\n");
+	        exit(1);
+	}
+
+
+	fprintf(fp, "Hello World\n");
+	*/
+	//while(1){
+		printf("Hello world\n");
+	//}
+	//fclose(fp);
+	return 0;
+
+}
+
