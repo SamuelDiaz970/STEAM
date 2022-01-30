bfeaba32cdc22968cf736850c210869e7a5937f8
Kanin-McGuire <85193693+Kanin-McGuire@users.noreply.github.com>
2022-01-30 10:55:18 -0700

    Add files via upload

--- STEAM_FlightSoftware/Unit_Building/Skeleton/Source/HMS.c
+++ STEAM_FlightSoftware/Unit_Building/Skeleton/Source/HMS.c
@@ -1,40 +1,40 @@
 // insert here
 
 Q7 Readings
-Read voltage from Q7
-Read current from Q7
-Read temp from Q7
-Read ADC current to Q7
-Read custom PCB temp
+Read voltage from Q7 - sensors (XADC)
+Read current from Q7 - sensors (XADC)
+Read temp from Q7 - sensors (XADC)
+Read ADC current to Q7 - not sensors (XADC)
+Read custom PCB temp - not sensors (XADC)
 
 DC-DC readings
-Read DC-DC Spacecraft Voltage
-Read DC-DC  Spacecraft Current
-Read DC-DC temp
-Read L-bracket temp
+Read DC-DC Spacecraft Voltage - ADC reads (i2c)
+Read DC-DC  Spacecraft Current - ADC reads (i2c)
+Read DC-DC temp - ADC reads (i2c)
+Read L-bracket temp - ADC reads (i2c)
 
 HXR Spectrometer Readings
-Read MOSFET 2 (HXR) voltage
-Read MOSFET 2 (HXR) current
-Read HXR detector temp
+Read MOSFET 2 (HXR) voltage  - ADC reads (i2c)
+Read MOSFET 2 (HXR) current  - ADC reads (i2c)
+Read HXR detector temp - RS232
 Decision
-Read HXR board temp
+Read HXR board temp  - RS232
 
 SXR Spectrometer Readings 
-Read MOSFET 1 (SXR) voltage
-Read MOSFET 1 (SXR) current
-Read SXR detector temp
+Read MOSFET 1 (SXR) voltage - ADC reads (i2c)
+Read MOSFET 1 (SXR) current - ADC reads (i2c)
+Read SXR detector temp  - RS232
 Decision
-Read SXR board temp
+Read SXR board temp  - RS232
 Decision
 
 PUNCH/EMI Temp Readings
-Read external connector temp
-Read surface wall temp
+Read external connector temp - ADC reads (i2c)
+Read surface wall temp - ADC reads (i2c)
 
 Package HMS data to HMS Log 
 
-Log any Errors
+Log any Errors 
 
 If Error threshold reached, notify main scheduler with error code
 
